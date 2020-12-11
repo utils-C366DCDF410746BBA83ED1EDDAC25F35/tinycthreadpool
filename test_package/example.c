41f486a36533220f477a5b54539e4c5fbc2eb6ef
bennyhuo <bennyhuo@kotliner.cn>
2020-12-12 07:46:50 +0800

    1. update example. Pushed to https://bintray.com/bennyhuo/conan-bennyhuo/tinycthreadpool%3Abennyhuo

--- test_package/example.c
+++ test_package/example.c
@@ -3,8 +3,12 @@
 #include <stdio.h>
 
 int main(void) {
+  puts("Start Testing!");
   threadpool_t *threadpool = threadpool_create(3, 5, 0);
-
-  threadpool_destroy(threadpool, threadpool_graceful);
+  printf("Create thread pool: %#x.\n", threadpool);
+  if (threadpool){
+    int result = threadpool_destroy(threadpool, 0);
+    printf("End Testing! result: %d\n", result);
+  }
   return 0;
 }
